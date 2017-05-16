4faf789caba62e509ffad6bef8eb577e734a408e
Vova <Vova@gmail.com>
2017-05-16 10:37:03 +0300

    origin

---
+++ Purse/BusinessCard/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BusinessCard
+{
+    public class BusinessCard
+    {
+        public string name = "";
+        public BusinessCard(string name)
+        {
+            this.name = name;
+        }
+        public string FindOutWhose()
+        {
+            return name + "'s business card";
+        }
+    }
+}
