d09f164377536abc8aead6a630e0663a794b7b93
Vova <Vova@gmail.com>
2017-05-16 10:21:09 +0300

    origin

---
+++ Purse/Money/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Money
+{
+    public class Money
+    {
+        private decimal amount = 0;
+        private string msg = "Money...pshhhh";
+        public string GetMoney()
+        {
+            return msg + amount.ToString();
+        }
+        public Money(decimal amount)
+        {
+            this.amount = amount;
+        }
+    }
+}
